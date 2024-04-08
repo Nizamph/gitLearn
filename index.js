4df6b5e0f651e133feb7762d0728119c5d1137b5
Nizamph <nizamph7@gmail.com>
2024-04-08 17:49:41 +0530

    new line from bug fix

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ console.log("my name is nizam");
 console.log("secons commmit from master");
 console.log("new line from remote");
 console.log("line 2 in remote");
+
+console.log("new line from bugFix branch");
